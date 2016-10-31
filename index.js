ce64167ee2fc91c5df47acd280a0b442024689a1
uriy <itemruby@gmail.com>
2016-10-31 21:15:47 +0300

    add layouts

--- index.js
+++ index.js
@@ -11,26 +11,22 @@ app.set('view engine', 'handlebars');
 
 app.set('port',process.env.PORT || 3000);
 
-app.get('/', function(req, res){
-    res.type('text/plain');
-    res.send('Meadowlark Travel');
+app.get('/', function(req, res) {
+    res.render('home');
 });
-app.get('/about', function(req, res){
-    res.type('text/plain');
-    res.send('О Meadowlark Travel');
+app.get('/about', function(req, res) {
+    res.render('about');
 });
-
-app.use(function (req,res) {
-    res.type('text/plain');
+// Обобщенный обработчик 404 (промежуточное ПО)
+app.use(function(req, res, next){
     res.status(404);
-    res.send('404 ничего не найдено');
+    res.render('404');
 });
-
-app.use(function (err,req,res,next) {
-    console.log(err.stack);
-    res.type('text/plain');
+// Обработчик ошибки 500 (промежуточное ПО)
+app.use(function(err, req, res, next){
+    console.error(err.stack);
     res.status(500);
-    res.send('500 ошибка сервера')
+    res.render('500');
 });
 
 app.listen(app.get('port'), function () {
