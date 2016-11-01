7e63e6962b422a23c3ee766538a4abbe4cf2d002
uriy <itemruby@gmail.com>
2016-11-01 21:59:27 +0300

    add form

--- index.js
+++ index.js
@@ -22,6 +22,20 @@ app.set('view engine', 'handlebars');
 app.set('port',process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
+app.use(require('body-parser').urlencoded({ extended: true }));
+
+app.get('/newsletter', function(req, res){
+// мы изучим CSRF позже... сейчас мы лишь
+// заполняем фиктивное значение
+    res.render('newsletter', { csrf: 'CSRF token goes here' });
+});
+app.post('/process' , function(req, res){
+    console.log('Form (from querystring): ' + req.query.form);
+    console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+    console.log('Name (from visible form field): ' + req.body.name);
+    console.log('Email (from visible form field): ' + req.body.email);
+    res.redirect(303, '/thank-you' );
+});
 
 app.get('/', function(req, res) {
     res.render('home');
